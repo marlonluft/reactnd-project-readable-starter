67621b9c0f29d7673e3e1145938944d586686d08
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-03-03 16:14:10 -0300

    feat: Ajustes listagem/exibição de categorias

--- frontend/src/Components/ListagemCategorias.js
+++ frontend/src/Components/ListagemCategorias.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class ListagemCategorias extends Component {
     render() {
@@ -12,7 +13,7 @@ class ListagemCategorias extends Component {
             <div>
                 {
                     Categorias.map((categoria) => (
-                        <div key={categoria.id}>{categoria.nome}</div>
+                        <Link key={categoria.id} to={'/categoria/'+categoria.id}>{categoria.nome}</Link>
                     ))
                 }
             </div>
--- frontend/src/Views/Categoria.js
+++ frontend/src/Views/Categoria.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import ListagemPostagens from '../Components/ListagemPostagens.js'
+import { Link } from 'react-router-dom'
 
 function Categoria({ match }) {
     return (
-      <div>
-        Categoria: {match.params.id}
-      </div>
+
+        <div>
+            <h2>{match.params.id}</h2>
+            <ListagemPostagens />
+            <Link to='/editar'>+Novo</Link>
+        </div>
     );
 }
 
