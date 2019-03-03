6fa3473e9725029371d32d60a103a59c2e264d74
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-03-03 16:42:26 -0300

    feat: Alterado tag li para Material Design

--- frontend/src/Components/Postagem.js
+++ frontend/src/Components/Postagem.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Postagem(props) {
     return (
-        <li>
-            <h3>{props.postagem.titulo}</h3>
-            <span>{props.postagem.dataCriacao}</span>
-            <span>{props.postagem.score}</span>
+        <li className="mdl-list__item mdl-list__item--two-line">
+            <span className="mdl-list__item-primary-content">
+                <span>{props.postagem.titulo}</span>
+            </span>
+            <span className="mdl-list__item-secondary-content">
+                <span>{props.postagem.score}</span>
+                <span className="mdl-list__item-sub-title">{props.postagem.dataCriacao}</span>
+            </span>
         </li>
     )
 }
