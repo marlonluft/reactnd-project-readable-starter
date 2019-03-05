4f9f0ab458bce56383bc62d6e9988aec546e45f5
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-03-04 21:31:10 -0300

    feat: mapeamento de rotas do sistema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Categoria from './Views/Categoria.js'
 import Detalhe from './Views/Detalhe.js'
-import Editar from './Views/Editar.js'
+import CadastrarEditarPostagem from './Views/CadastrarEditarPostagem.js'
 import Root from './Views/Root.js'
-import { Route } from 'react-router-dom'
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="app">
-        <Route path='/categoria/:id' component={Categoria} />
-        <Route path='/detalhe/:id' component={Detalhe} />
-        <Route path='/editar/:id' component={Editar} />
-        <Route exact path='/' component={Root} />
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/categoria/:id' component={Categoria} />
+            <Route path='/detalhe/:id' component={Detalhe} />
+            <Route path='/editar/:id' component={CadastrarEditarPostagem} />
+            <Route path='/cadastrar' component={CadastrarEditarPostagem} />
+            <Route exact path='/' component={Root} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default App
